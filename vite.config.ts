bfcbd6cb8d26e3ea97a39ed66f2bca97380099ff
rashedrk <rashedrk001@gmail.com>
2026-01-14 00:21:46 +0600

    setup fix and dom test by text

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
